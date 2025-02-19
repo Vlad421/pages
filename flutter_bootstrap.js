c79d4731b4ec7634f0ffbf39fa0875773602ffe4
Vlad421 <35481094+Vlad421@users.noreply.github.com>
2025-02-19 14:55:26 -0700

    - fonts - fixed alignment

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "662100095"
+    serviceWorkerVersion: "4260794737"
   }
 });
